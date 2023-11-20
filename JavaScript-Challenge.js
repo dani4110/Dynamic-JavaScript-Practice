af42aecf1004f4e72fa4c9d83fcb46c07fafe40f
dani4110 <freakoutcoding@gmail.com>
2023-11-20 11:16:12 -0500

    myNameUpperCase

--- JavaScript-Challenge.js
+++ JavaScript-Challenge.js
@@ -2,4 +2,10 @@
 
 // 1.- Hello World! I'm Daniel.
 
-console.log('Hello World! I\'m Daniel.');
+// console.log('Hello World! I\'m Daniel.');
+
+// 2.- My name in UpperCase is DANIEL.
+
+const myName = 'Daniel';
+const myNameUpperCase = myName.toUpperCase();
+console.log('My name in UpperCase is :' + myNameUpperCase);
